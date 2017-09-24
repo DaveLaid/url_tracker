f92c8f1c049bb83ebc399be9844917613fc2372f
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-09-24 13:11:48 -0700

    components-3

---
+++ app/components/Home.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import Search from "./common/Search";
+import Add from "./common/Add";
+import Saved from "./common/Saved"; 
+import API from "../utils/API";
+
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      something: []
+    };
+    // Binding getUrls to our component since we'll be passing this
+    // method to child components
+    {/*this.getUrls = this.getUrls.bind(this);*/}
+  }
+  // Getting all Urls when the component mounts
+  componentDidMount() {
+    {/*this.getUrls();*/}
+  }
+  getUrls() {
+    {/*API.getUrls().then((res) => {
+      this.setState({ Urls: res.data });
+    });*/}
+  }
+  // A helper method for rendering one panel for each url
+  renderUrls() {
+    {/*return this.state.Urls.map(url => (
+      <Panel
+        url={url}
+        key={url._id}
+        getUrls={this.getUrls}
+      />
+    )); */}
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <Search
+
+          />
+        </div>
+
+
+        <div className="row">
+          <Add
+
+          />
+        </div>
+
+
+        <div className="row">
+          <Saved
+          
+          />
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ app/components/common/Saved.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import API from "../../utils/API";
+
+class Saved extends Component {
+
+	render() {
+	    
+	    return (
+
+	    	<div className="row">SAVED component placeholder
+	        </div>
+	    
+	    );
+
+	}
+}
+
+export default Saved;
