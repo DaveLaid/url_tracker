733b0dbd7863eeb080877eb48e3305f0c63adf6a
Anita Brument <Anita@Anitas-MacBook-Pro.local>
2017-09-25 15:05:39 -0700

    route

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,13 +3,24 @@ import { Route, IndexRoute, Router, browserHistory } from "react-router";
 
 import Main from "../components/Main";
 import Home from "../components/Home";
+import Signinup from "../components/Signinup";
 
 
-
+/*
 const routes = (
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
     	<IndexRoute component={Home} />
+    </Route>
+  </Router>
+);
+*/
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+    	<Route path="home" component={Home} />
+    	<IndexRoute component={Signinup} />
     </Route>
   </Router>
 );
